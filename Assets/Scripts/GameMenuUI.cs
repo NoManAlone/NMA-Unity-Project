c6cc94511ffeb8bf11a24c359fe1a7ca7bc7207b
DonnchadhOF <donnchadhof@gmail.com>
2015-09-29 02:43:03 +0100

    Start Menu and Game Lobby
    Created basic start menu and game lobby.
    Created basic in game menu.

---
+++ Assets/Scripts/GameMenuUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameMenuUI : MonoBehaviour
+{
+    public Transform menuPanel;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(menuPanel.gameObject.activeSelf)           
+                CloseMenu();
+            
+            else
+                OpenMenu();
+        }
+    }
+
+    public void OpenMenu()
+    {
+        menuPanel.gameObject.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        menuPanel.gameObject.SetActive(false);
+    }
+
+    public void MainMenu()
+    {
+        PhotonNetwork.Disconnect();
+        Application.LoadLevel("Start Screen");
+    }
+}
---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour
+{
+    //Store list of created games
+    RoomInfo[] gamesList;
+
+    public GameObject lobbyUI;
+
+    // Use this for initialization
+    void Start ()
+    {
+        PhotonNetwork.ConnectUsingSettings("v4.2");      
+    }
+
+    void OnConnectedToPhoton()
+    {
+        Debug.Log("Connected to Photon");
+    }
+
+    void OnFailedToConnectToPhoton()
+    {
+        Debug.Log("Failed to connect to Photon");
+    }
+
+    ////Called if Auto-Join Lobby is true in PhotonServerSettings asset
+    //void OnJoinedLobby()
+    //{
+    //    Debug.Log("Joined Lobby");
+
+    //    RoomOptions roomOptions = new RoomOptions() { isVisible = false, maxPlayers = 2 };
+    //    PhotonNetwork.JoinOrCreateRoom("Game", roomOptions, TypedLobby.Default);
+    //}
+
+
+    public void CreateRoom(GameObject inputField)
+    {
+        string gameName = inputField.GetComponent<InputField>().text;
+
+        RoomOptions roomOptions = new RoomOptions() { isVisible = true, maxPlayers = 2 };
+        PhotonNetwork.CreateRoom(gameName, roomOptions, TypedLobby.Default);
+    }
+
+    public void JoinRoom(GameObject inputField)
+    {
+        string gameName = inputField.GetComponent<InputField>().text;
+
+        PhotonNetwork.JoinRoom(gameName);
+    }
+
+    //Called after OnJoinedLobby
+    void OnReceivedRoomListUpdate()
+    {
+        //Refresh games list on joined lobby
+        lobbyUI.GetComponent<LobbyUIManager>().PopulateGamesList();
+    }
+
+    void OnJoinedRoom()
+    {
+        Debug.Log("Joined Room");
+
+        PhotonNetwork.isMessageQueueRunning = false;
+
+        Application.LoadLevel("Donnchadh Test");
+    }
+}
